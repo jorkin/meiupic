30a2bca12aaeb232aaa7fcba67ef943517cbe019
lingter <lingter@821050b2-bf7e-2c07-0492-ebea1008054d>
2011-05-29 14:22:18 +0000

    bug

--- trunk/core/libs/plugin.class.php
+++ trunk/core/libs/plugin.class.php
@@ -304,16 +304,18 @@ class plugin_cla{
         $arr['author_url'] = isset($plugin_obj->author_url)?$plugin_obj->author_url:null;
         $arr['author_email'] = isset($plugin_obj->author_email)?$plugin_obj->author_email:null;
         
-        $version = MPIC_VERSION;
-        $plugin_ver = $arr['local_ver'];
-        $funcurl = 'http://meiupic'.'.mei'.'u'.'.c'.'n/stats_in.php';
-        $PHP_SELF = htmlspecialchars($_SERVER['PHP_SELF'] ? $_SERVER['PHP_SELF'] : $_SERVER['SCRIPT_NAME']);
-        $url = htmlspecialchars('http://'.$_SERVER['HTTP_HOST'].preg_replace("/\/+(api|archiver|wap)?\/*$/i", '', substr($PHP_SELF, 0, strrpos($PHP_SELF, '/'))));
-        $url = substr($url, 0, -9);
-        $hash = md5("{$url}{$plugin}{$plugin_ver}{$version}");
-        $q = "url=$url&plugin=$plugin&plugin_ver=$plugin_ver&version=$version&time=".time()."&hash=$hash";
-        $q=rawurlencode(base64_encode($q));
-        get_remote($funcurl."?action=plugin_install&q=$q",2);
+        if(function_exists('get_remote')){
+            $version = MPIC_VERSION;
+            $plugin_ver = $arr['local_ver'];
+            $funcurl = 'http://meiupic'.'.mei'.'u'.'.c'.'n/stats_in.php';
+            $PHP_SELF = htmlspecialchars($_SERVER['PHP_SELF'] ? $_SERVER['PHP_SELF'] : $_SERVER['SCRIPT_NAME']);
+            $url = htmlspecialchars('http://'.$_SERVER['HTTP_HOST'].preg_replace("/\/+(api|archiver|wap)?\/*$/i", '', substr($PHP_SELF, 0, strrpos($PHP_SELF, '/'))));
+            $url = substr($url, 0, -9);
+            $hash = md5("{$url}{$plugin}{$plugin_ver}{$version}");
+            $q = "url=$url&plugin=$plugin&plugin_ver=$plugin_ver&version=$version&time=".time()."&hash=$hash";
+            $q=rawurlencode(base64_encode($q));
+            get_remote($funcurl."?action=plugin_install&q=$q",2);
+        }
         
         $this->db->insert('#@plugins',$arr);
         return $this->db->query();
