a28003b4534b5248c89521b9409c3ff98e2b3fe3
lingter <lingter@gmail.com>
2012-07-16 15:20:49 +0000

    模版首页。 gd翻转图片时加入一个判断

--- themes/sense/lang/zh_cn.lang.php
+++ themes/sense/lang/zh_cn.lang.php
@@ -1,4 +1,6 @@
 <?php
-$language['new_pictures'] =  '最新图片';
+$language['new_photos'] =  '最新照片';
 $language['new_albums']   =  '最新相册';
 $language['all_albums']   =  '所有相册';
+$language['new_comment_for_albums'] = '最新相册评论';
+$language['new_comment_for_photos'] = '最新照片评论';
